7079e5c3c319fc4fd36824a3af7ecb3fbc3e7e96
Geetha <danda.saigeetha@wipro.com>
2024-04-21 09:55:14 +0000

    added files

---
+++ hellow_jenkins.java
@@ -0,0 +1,16 @@
+ 
+public class hellow_jenkins
+
+{
+
+    public static void main( String[] args )
+
+    {
+
+       String str = "Hello jenkins!";
+
+       System.out.println(str);
+
+    }
+
+}
---
+++ hellow_wipro.java
@@ -0,0 +1,16 @@
+ 
+public class hello_wipro
+
+{
+
+    public static void main( String[] args )
+
+    {
+
+       String str = "Hello wipro!";
+
+       System.out.println(str);
+
+    }
+
+}
